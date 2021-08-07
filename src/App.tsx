7efc54bdc145e7a85fdaaa5675db8d9e8078ef31
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-07 11:23:42 +0530

    Setup eslint, bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
+import 'assets/App.scss';
 
-function App() {
-  return (
-    <div >
-      student companion
-    </div>
-  );
-}
+const App: FC<{}> = () => (
+  <div className="row">
+    <div className="col-md-6">Student</div>
+    <div className="col-md-6">Companion</div>
+  </div>
+);
 
 export default App;
