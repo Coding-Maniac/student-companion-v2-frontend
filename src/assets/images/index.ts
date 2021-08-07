cac4ffef99c8f81da01fb4cd802ccaf18e6d644e
Sanjay Kapilesh <ufoundsanjay@gmail.com>
2021-08-07 13:14:11 +0530

    Login page first cut

---
+++ src/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as LeftBanner } from './Scholarbuddy_landing_page_poster.png';
--- src/components/LoginLeftSide.tsx
+++ src/components/LoginLeftSide.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import { LeftBanner } from 'assets/images';
 
-const LoginLeftSide: FC<{}> = () => <div className="app_login_left_side">Login Left side</div>;
+const LoginLeftSide: FC<{}> = () => (
+  <div className="app_login_left_side">
+    <img src={LeftBanner} alt="Scholar Buddy Landing" className="app_left_banner" />
+  </div>
+);
 
 export default LoginLeftSide;
